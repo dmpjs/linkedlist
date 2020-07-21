fa57a781a8c4435620a11cc84fe88cd744020c36
Daniel Bannert <prisis@users.noreply.github.com>
2020-07-21 12:14:40 +0200

    Initial commit

---
+++ release.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  "dryRun": false,
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventionalcommits",
+      },
+    ],
+    [
+      "@google/semantic-release-replace-plugin",
+      {
+        "replacements": [
+          {
+            "files": [
+              "version.ts",
+            ],
+            "from": 'const VERSION = ".*"',
+            "to":
+              'const VERSION = "${nextRelease.version}"',
+            "results": [
+              {
+                "file": "version.ts",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1,
+              },
+            ],
+            "countMatches": true,
+          },
+          {
+            "files": [
+              "README.md",
+            ],
+            "from": '/v.*/',
+            "to":
+              '/v${nextRelease.version}/',
+            "results": [
+              {
+                "file": "README.md",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1,
+              },
+            ],
+            "countMatches": true,
+          },
+          {
+            "files": [
+              "README.md",
+            ],
+            "from": '@.*/',
+            "to":
+              '@${nextRelease.version}/',
+            "results": [
+              {
+                "file": "README.md",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1,
+              },
+            ],
+            "countMatches": true,
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        "preset": "conventionalcommits",
+      },
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "version.ts",
+          "mod.ts",
+          "src/*",
+          "UPGRADE.md",
+          "LICENSE.md",
+          "CHANGELOG.md",
+        ],
+      },
+    ],
+  ],
+};
---
+++ test_deps.ts
@@ -0,0 +1,5 @@
+export {
+  assertEquals,
+  assertThrows,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.61.0/testing/asserts.ts";
