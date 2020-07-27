542a617d2b2e72e39fba04e14ea0395c9f615eaf
prisis <d.bannert@anolilab.de>
2020-07-27 11:40:20 +0200

    fix: fixed matches on release.config.js

--- release.config.js
+++ release.config.js
@@ -31,8 +31,8 @@ module.exports = {
             "files": [
               "README.md",
             ],
-            "from": "/v*/",
-            "to": "/v${nextRelease.version}/",
+            "from": "\/v*\/",
+            "to": "\/v${nextRelease.version}\/",
             "results": [
               {
                 "file": "README.md",
@@ -47,8 +47,8 @@ module.exports = {
             "files": [
               "README.md",
             ],
-            "from": "@*/",
-            "to": "@${nextRelease.version}/",
+            "from": "@*\/",
+            "to": "@${nextRelease.version}\/",
             "results": [
               {
                 "file": "README.md",
